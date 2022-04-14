ae620311d0edaeab2eeac81a85ee5d06ac5a263c
SorokaYago <sorokayago@gmail.com>
2022-04-14 13:15:30 -0300

    testing

--- TP1LAB12022/src/TP1LAB12022.c
+++ TP1LAB12022/src/TP1LAB12022.c
@@ -4,11 +4,11 @@
  Author      : sorokaYago
  Copyright   : 2022
  Ejercicio   : agencia de viaje necesita calcular costos para sus vuelos de
- Latam y Aerolíneas Argentinas para ofrecerlos a sus clientes.
-Se deberá ingresar por cada vuelo los km totales y el precio total del mismo.
-El objetivo de la aplicación es mostrar las diferentes opciones de
-pagos a sus clientes. Para ello el programa iniciará y
-contará con un menú de opciones
+ Latam y Aerolneas Argentinas para ofrecerlos a sus clientes.
+Se deber ingresar por cada vuelo los km totales y el precio total del mismo.
+El objetivo de la aplicacin es mostrar las diferentes opciones de
+pagos a sus clientes. Para ello el programa iniciar y
+contarcon un men de opciones
  ============================================================================
  */
 
